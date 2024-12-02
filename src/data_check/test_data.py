49c3933d52a3ce02d59d1339046063e49ae30fa8
David <drob946@wgu.edu>
2024-12-02 12:06:36 -0600

    test_data

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,12 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """Ensure dataset has a reasonable number of rows."""
+    assert 15000 < data.shape[0] < 1000000, "Dataset row count is outside the acceptable range."
+
+def test_price_range(data, min_price, max_price):
+    """Check that prices are within the given range."""
+    assert data['price'].between(min_price, max_price).all(), \
+        "Some prices are outside the specified range."
